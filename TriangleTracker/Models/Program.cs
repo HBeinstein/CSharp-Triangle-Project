788188abf46ed79db066884d65f1bd36e6f1e6db
Spencer Moody <spencer.moody@outlook.com>
2020-07-14 13:33:57 -0700

    Add output for triangle types
    Co-authored-by: Hannah Beinstein <hannah.beinstein@gmail.com>

--- TriangleTracker/Models/Program.cs
+++ TriangleTracker/Models/Program.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Collections.Generic;
+using TriangleTracker;
 
 namespace TriangleProject
 {
@@ -7,14 +9,17 @@ namespace TriangleProject
     public static void Main()
     {
       Console.WriteLine("Enter Side One");
-      int sideOne = int.Parse(Console.ReadLine());
+      int side1 = int.Parse(Console.ReadLine());
       
       Console.WriteLine("Enter Side Two");
-      int sideTwo = int.Parse(Console.ReadLine());
+      int side2 = int.Parse(Console.ReadLine());
 
       Console.WriteLine("Enter Side Three");
-      int sideThree = int.Parse(Console.ReadLine());
+      int side3 = int.Parse(Console.ReadLine());
 
+      Triangle triangleType = new Triangle(side1, side2, side3);
+      string triangleName = triangleType.WhatTypeTriangle(side1, side2, side3);
+      Console.WriteLine(triangleName);
     }
   }
 }
--- TriangleTracker/Models/Triangle.cs
+++ TriangleTracker/Models/Triangle.cs
@@ -1,10 +1,84 @@
-namespace Calendar
+namespace TriangleTracker
 {
-  public class LeapYear
+  public class Triangle
   {
-    public bool IsLeapYear(int year)
+    public int Side1 { get; set; }
+    public int Side2 { get; set; }
+    public int Side3 { get; set; }
+
+    public Triangle(int side1, int side2, int side3)
     {
-      return year % 4 == 0;
+      Side1 = side1;
+      Side2 = side2;
+      Side3 = side3;
     }
-  }
+
+    public bool IsTriangleEquilateral(int side1, int side2, int side3)
+    {
+      if (side1 == side2 && side1 == side3)
+      {
+        return true;
+      }
+        else  
+      {
+        return false;
+      }
+    }
+
+    public bool IsTriangleIsosceles(int side1, int side2, int side3)
+    {
+      if (side1 == side2 || side1 == side3 || side2 == side3)
+      {
+        return true;
+      }
+        else  
+      {
+        return false;
+      }
+    }
+
+    public bool IsTriangleScalene(int side1, int side2, int side3)
+    {
+      if (side1 != side2 && side1 != side3 && side2 != side3)
+      {
+        return true;
+      }
+        else  
+      {
+        return false;
+      }
+    }
+
+    public bool IsNotTriangle(int side1, int side2, int side3)
+    {
+      if (((side1 + side2) < side3) || ((side2 + side3) < side1) || ((side3 + side1) < side2))
+      {
+        return true;
+      }
+        else
+      {
+        return false;
+      }  
+    }
+
+      public string WhatTypeTriangle(int side1, int side2, int side3)
+    {
+      if (IsNotTriangle(side1, side2, side3))
+      {
+        return "not a triangle";
+      }
+      else if (IsTriangleEquilateral(side1, side2, side3))
+      {
+        return "equilateral";
+      }
+      else if (IsTriangleIsosceles(side1, side2, side3))
+      {
+        return "isosceles";
+      }
+      else
+      {
+        return "scalene";
+      }
+    }  
+  }  
 }
