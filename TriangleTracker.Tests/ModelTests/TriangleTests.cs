d62d40d960c1b4595d0365ce7301c9737afd0c43
Spencer Moody <spencer.moody@outlook.com>
2020-07-14 11:16:13 -0700

    Pass test for IsTriangleIsosceles
    Co-authored-by: Hannah Beinstein <hannah.beinstein@gmail.com>

--- TriangleTracker.Tests/ModelTests/TriangleTests.cs
+++ TriangleTracker.Tests/ModelTests/TriangleTests.cs
@@ -12,5 +12,12 @@ namespace Tests
       Triangle testEquilateral = new Triangle();
       Assert.AreEqual(true, testEquilateral.IsTriangleEquilateral(3, 3, 3));
     }
+
+    [TestMethod]
+    public void IsTriangleIsosceles_Isosceles_True()
+    {
+      Triangle testIsosceles = new Triangle();
+      Assert.AreEqual(true, testIsosceles.IsTriangleIsosceles(3, 3, 2));
+    }
   }
 }
